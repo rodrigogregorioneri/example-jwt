94c63c3b2254e019fd862a70672c55c6fcf2a05d
neridev <rodrigogregorioneri@hotmail.com>
2019-03-11 17:28:54 -0300

    Criando um userDetails customizado o mesmo extend de UserDetails

---
+++ src/main/java/com/neri/jwt/nerijwt/security/UserDetailsImpl.java
@@ -0,0 +1,58 @@
+package com.neri.jwt.nerijwt.security;
+
+import com.neri.jwt.nerijwt.model.User;
+import com.neri.jwt.nerijwt.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserDetailsImpl implements UserDetails {
+
+    User user;
+    private List<GrantedAuthority> roles;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+        this.roles = roles;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
