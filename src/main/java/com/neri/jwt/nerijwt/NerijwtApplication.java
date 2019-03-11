d811a0ea7f396a3bd19a3049680f4e88a55bfc13
neridev <rodrigogregorioneri@hotmail.com>
2019-03-11 18:04:29 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/neri/jwt/nerijwt/model/User.java

--- src/main/java/com/neri/jwt/nerijwt/NerijwtApplication.java
+++ src/main/java/com/neri/jwt/nerijwt/NerijwtApplication.java
@@ -1,13 +1,43 @@
 package com.neri.jwt.nerijwt;
 
+import com.neri.jwt.nerijwt.model.User;
+import com.neri.jwt.nerijwt.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.UUID;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class NerijwtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NerijwtApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+		return args -> {
+			initUsers(userRepository, passwordEncoder);
+		};
+
+	}
+
+	private void initUsers(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+		User user = new User();
+
+
+
+		user.setId(UUID.randomUUID().toString().toUpperCase());
+		user.setEmail("rodrigogregorioneri@hotmail.com");
+		user.setPassword(passwordEncoder.encode("admin"));
+
+		User find = userRepository.findByEmail("rodrigogregorioneri@hotmail.com");
+		if (find == null) {
+			userRepository.save(user);
+		}
 	}
 
 }
