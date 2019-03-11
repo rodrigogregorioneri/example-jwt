6381f0936ed84ded7e3191b3fefd5d71e51e546e
neridev <rodrigogregorioneri@hotmail.com>
2019-03-11 16:03:48 -0300

    Initial commit

---
+++ src/main/java/com/neri/jwt/nerijwt/NerijwtApplication.java
@@ -0,0 +1,13 @@
+package com.neri.jwt.nerijwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerijwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerijwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neri/jwt/nerijwt/NerijwtApplicationTests.java
@@ -0,0 +1,16 @@
+package com.neri.jwt.nerijwt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NerijwtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
