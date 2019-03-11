9c482c0cda6a2016576bcc23509fbfa9fec7f71e
neridev <rodrigogregorioneri@hotmail.com>
2019-03-11 17:41:28 -0300

    Criando filtro de token necessario pra autenticação JWT, e classes utilitarias JWT e adicionando esse filtro no SecurityConfig

--- src/main/java/com/neri/jwt/nerijwt/model/User.java
+++ src/main/java/com/neri/jwt/nerijwt/model/User.java
@@ -1,5 +1,6 @@
 package com.neri.jwt.nerijwt.model;
 
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -16,4 +17,43 @@ public class User {
     private  String name;
     private  String password;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ProfileEnum getProfile() {
+        return profile;
+    }
+
+    public void setProfile(ProfileEnum profile) {
+        this.profile = profile;
+    }
 }
